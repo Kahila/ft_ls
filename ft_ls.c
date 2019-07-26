87db722d20513c4739f827b2862554acbce13e37
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-07-26 17:56:34 +0200

    28/07/2019

--- ft_ls.c
+++ ft_ls.c
@@ -6,26 +6,64 @@
 /*   By: akalombo <akalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/23 16:51:16 by akalombo          #+#    #+#             */
-/*   Updated: 2019/07/24 13:10:49 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/07/26 17:55:09 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_ls.h"
 
-int no_flags(int argc, struct dirent *files, DIR *mydir)
+//method will be used to sort the names of the functions lexicographically
+void insert_sort()
+{
+
+	return;
+}
+
+//this functions will mimic the ls command without the any flags
+void 				no_flags(int argc, struct dirent *files, DIR *mydir)
 {
 	if (argc == 1)
 	{
+		int i = 0;
+
 		while ((files = readdir(mydir)) != NULL)
 		{
+			//will not display any hidden folders
 			if (files->d_name[0] != '.')
 			{
+				int l = 0;
+				i = 0;
+				while (files->d_name[i])
+				{
+					l += files->d_name[i];
+					i++;
+				}
 				ft_putstr(files->d_name);
 				ft_putchar('\t');
 			}
 		}
-		return (1);
 	}
-	return (0);
+}
+
+//this method will mimic the ls commnad with the -a flag
+void					flag_hidden(int argc, char *argv,struct dirent *files, DIR *mydir)
+{
+	if (argc == 2 && argv[0] == POTENTIAL_FLAG && argv[1] == HIDDEN_FILES)
+	{
+		int i = 0;
+		while ((files = readdir(mydir)) != NULL)
+		{
+			int l = 0;
+			i = 0;
+			while (files->d_name[i])
+			{
+				l += files->d_name[i];
+				i++;
+			}
+			//desplaying all folders including hidden folders
+			ft_putstr(files->d_name);
+			ft_putchar('\t');
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -34,6 +72,7 @@ int main(int argc, char **argv)
 	struct dirent *files;
 	mydir = opendir(".");
 	no_flags(argc, files, mydir);
+	flag_hidden(argc, argv[1], files, mydir);
 	closedir(mydir);
-	return (0);
+	return (SUCCESS);
 }
--- ft_ls.h
+++ ft_ls.h
@@ -6,25 +6,31 @@
 /*   By: akalombo <akalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/24 12:38:54 by akalombo          #+#    #+#             */
-/*   Updated: 2019/07/24 13:30:23 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/07/26 17:55:06 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 #define FT_LS_H
 
-#include "libft/libft.h"
-#include <unistd.h>
-#include <time.h>      //working with the time and the ctime functions
-#include <dirent.h>    //working with the opendir, readdir and closedir functions
-#include <sys/stat.h>  //working with the stat function and lstat functions
-#include <sys/types.h> //
-#include <pwd.h>       //working with the passwd struct;
-#include <uuid/uuid.h> //working with the uuid_t data type
-#include <sys/xattr.h> //working with the listaxttr and the getxattr functions
-#include <stdlib.h>    //working with the malloc, exit and free functions
-#include <stdio.h>     //working with the perror function
-#include <string.h>    //working with the strerror function
+# include "libft/libft.h"
+# include <unistd.h>
+# include <time.h>      //working with the time and the ctime functions
+# include <dirent.h>    //working with the opendir, readdir and closedir functions
+# include <sys/stat.h>  //working with the stat function and lstat functions
+# include <sys/types.h> //
+# include <pwd.h>       //working with the passwd struct;
+# include <uuid/uuid.h> //working with the uuid_t data type
+# include <sys/xattr.h> //working with the listaxttr and the getxattr functions
+# include <stdlib.h>    //working with the malloc, exit and free functions
+# include <stdio.h>     //working with the perror function
+# include <string.h>    //working with the strerror function
+# define POTENTIAL_FLAG '-'
+# define HIDDEN_FILES 'a'
+# define FILE_DETAILS 'l'
+# define RECURSIVE_LS 'R'
+# define SORT_LAST_MODI 't'
+# define SORT_REV_LEXO 'r'
 enum output
 {
     SUCCESS,
