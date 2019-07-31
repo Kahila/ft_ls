590947539087c30d47bd008b15da2b435f07d829
Ado KALOMBO <akalombo@c4r10s6.wethinkcode.co.za>
2019-07-31 15:37:11 +0200

    31/08/2019

--- ft_ls.c
+++ ft_ls.c
@@ -6,88 +6,108 @@
 /*   By: akalombo <akalombo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/23 16:51:16 by akalombo          #+#    #+#             */
-/*   Updated: 2019/07/26 21:30:15 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/07/31 14:41:13 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "ft_ls.h"
 #include <stdio.h>//remember to remove
 
-//last thought ... to get string that will store all the file/dir names and then split using str split .... then sort the list in lexicographical order ... then display from the ** that contains the files
-
-
-//method will be used to sort the names of the functions lexicographically
-void insert_sort()
+//method will be used desplay the content within the directory without a flag
+void				no_flags(int argc, char **content)
 {
+	int i;
 
+	if (argc == 1)
+	{
+		i = 0;
+		while (content[i])
+		{
+			if (content[i][0] != '.')
+			{
+				ft_putstr(content[i]);
+				ft_putchar('\t');
+			}
+			i++;
+		}
+	}
 	return;
 }
 
-//this functions will mimic the ls command without the any flags
-int 				no_flags(int argc, struct dirent *files, DIR *mydir)
-{
-	int tot = 0;
-	if (argc == 1)
-	{
-		int i = 0;
-		int l = 0;;
-		while ((files = readdir(mydir)) != NULL)
-		{
-			//will not display any hidden folders
-			if (files->d_name[0] != '.')
-			{
-				i = 0;
-				while (files->d_name[i])
-				{
-					l += files->d_name[i];
-					//printf("%c/", files->d_name[i]);
-					i++;
-				}
-				ft_putstr(files->d_name);
-				ft_putchar('\t');
-				tot++;
-			}
-		}
-		return (tot);
-	}
-	return (0);
-}
-
-//this method will mimic the ls commnad with the -a flag
-int					flag_hidden(int argc, char *argv,struct dirent *files, DIR *mydir)
-{
-	int tot = 0;
-	if (argc == 2 && argv[0] == POTENTIAL_FLAG && argv[1] == HIDDEN_FILES)
-	{
-		int i = 0;
-		int l = 0;
-		while ((files = readdir(mydir)) != NULL)
-		{
-			i = 0;
-			while (files->d_name[i])
-			{
-				l += files->d_name[i];
-				i++;
-			}
-			//desplaying all folders including hidden folders
-			ft_putstr(files->d_name);
-			ft_putchar('\t');
-			tot++;
-		}
-		return (tot);
-	}
-	return (0);
-}
-
-int main(int argc, char **argv)
+//this method will display all files including hidden ones
+void				hidden_flag(int argc, char **content, char *argv)
 {
 	int i;
+
+	if (argc > 1 && argv[0] == POTENTIAL_FLAG && argv[1] == HIDDEN_FILES)
+	{
+		i = 0;
+		while (content[i])
+		{
+			ft_putstr(content[i]);
+			ft_putchar('\t');
+			i++;
+		}
+	}
+	return;
+}
+
+//this method will save all the ontent names which will be sorted
+void			get_content(int tot, int argc, char *argv)
+{
+	static char **content;
+	static int j = 0;
 	DIR *mydir;
 	struct dirent *files;
 	struct dirent *count;
 	mydir = opendir(".");
-	no_flags(argc, files, mydir);
-	flag_hidden(argc, argv[1], files, mydir);
-	ft_putnbr(i);
+	content = (char **)malloc(sizeof(char *) * (tot + 1));
+	int i = 0;
+	int l = 0;
+	while ((files = readdir(mydir)) != NULL)
+	{
+		i = 0;
+		while (files->d_name[i])
+		{
+			l += files->d_name[i];
+			i++;
+		}
+		content[j] = ft_strdup(files->d_name);
+		j++;
+	}
+	closedir(mydir);
+	no_flags(argc, content);
+	hidden_flag(argc, content, argv);
+	content[tot + 1] = NULL;
+	return;
+}
+
+//this method will count the amount of content within the ./
+void			count_content(struct dirent *files, DIR *mydir, int argc, char *argv)
+{
+	int tot = 0;
+	int i = 0;
+	int l = 0;
+	while ((files = readdir(mydir)) != NULL)
+	{
+		i = 0;
+		while (files->d_name[i])
+		{
+			l += files->d_name[i];
+			i++;
+		}
+		tot++;
+	}
+	get_content(tot, argc, argv);
+}
+
+int main(int argc, char **argv)
+{
+	DIR *mydir;
+	struct dirent *files;
+	struct dirent *count;
+	
+	mydir = opendir(".");
+	count_content(files, mydir, argc,argv[1]);
 	closedir(mydir);
 	return (SUCCESS);
 }
