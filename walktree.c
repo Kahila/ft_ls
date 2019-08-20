14806f27706a9ad0bce06fec52b56d25bd188bae
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-08-20 12:13:31 +0200

    dir tree

--- walktree.c
+++ walktree.c
@@ -10,6 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_ls.h"
+
+//method that will be used to get the list of dir in thet curr dir
+char **get_dir(t_list *lst, char *folder, t_list *head)
+{
+    char **dir;
+    int len;
+    int i;
+
+    i = 0;
+    len = count_dir(lst, head, folder);
+    dir = (char **)malloc(sizeof(char *) * len + 1);
+    dir[len + 1] = NULL;
+    while (i < len)
+    {
+        if (lst->dirORfile == IS_FOLDER)
+        {
+            dir[i] = ft_strdup(lst->fileName);
+            i++;
+        }
+        lst = lst->prev;
+    }
+    return (dir);
+}
+
+//method will be used to get the paths of the files in subdir
+
 //method will be used to walk the directory tree
 //will display the content of all the directories that have been walked
-
