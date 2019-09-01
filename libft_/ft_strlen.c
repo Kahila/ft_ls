458672aeb1115c95a5333f97a3ac5cc3d328ff00
Adonis Kahila <46842993+Kahila@users.noreply.github.com>
2019-09-01 06:55:05 -0700

    Add files via upload

--- libft_/ft_strlen.c
+++ libft_/ft_strlen.c
@@ -12,12 +12,15 @@
 
 #include "libft.h"
 
-size_t				ft_strlen(const char *str)
+size_t ft_strlen(const char *str)
 {
 	size_t counter;
 
 	counter = 0;
-	while (str[counter] != 0)
-		counter++;
+	if (str)
+	{
+		while (str[counter] != '\0')
+			counter++;
+	}
 	return (counter);
 }
