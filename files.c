5c81477c6ee430a481e74eed03e8c895db13ca0e
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-09-18 15:59:56 +0200

    parsing files

--- files.c
+++ files.c
@@ -1,4 +1,15 @@
-#include "ft_ls.h" 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   files.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalombo <akalombo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/18 11:58:43 by akalombo          #+#    #+#             */
+/*   Updated: 2019/09/18 11:58:46 by akalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "ft_ls.h"
 
 //last action ... saving the file within the path
 
@@ -11,12 +22,13 @@ char *split_path(char *dir)
 	char *path;
 
 	j = i = 0;
-	while (dir[i])
-		i++;
+	// while (dir[i])
+	// 	i++;
+	i = ft_strlen(dir);
 	while (dir[i] != '/')
 		i--;
 	stop = i--;
-	path = (char *)malloc(sizeof(char) *(i + 1));
+	path = (char *)malloc(sizeof(char) * (i + 1));
 	path[i + 1] = '\0';
 	i = 0;
 	while (j != stop)
@@ -45,36 +57,39 @@ int path_(char *str)
 	return (0);
 }
 
-//method that will get the path
-char *split_file(char *dir)
-{
-	int i;
-	int j;
-	int tot;
-	char *new;
+// //method that will get the path
+// char *split_file(char *dir)
+// {
+// 	int i;
+// 	int j;
+// 	int tot;
+// 	char *new;
 
-	tot = j = i = 0;
-	while (dir[i])
-		i++;
-	i--;
-	while (dir[i] != '/')
-	{
-		tot++;
-		i--;
-	}
-	new = (char *)malloc(sizeof(char)*(tot));
-	new[tot] = '\0';
-	printf("j = %d\n", j);
-	while (new[j])
-	{
-		printf("here in\n");
-		new[j] = dir[i];
-		printf("(%c)\n", new[j]);
-		i++;
-		j++;
-	}
-	printf("file == %s\n", new);
-}
+// 	tot = j = i = 0;
+// 	while (dir[i])
+// 		i++;
+// 	i--;
+// 	while (dir[i] != '/')
+// 	{
+// 		tot++;
+// 		i--;
+// 	}
+// 	printf(">>>>>>>>>>here tot = %d\n", tot);
+// 	new = (char *)malloc(sizeof(char) * (tot));
+// 	new[tot] = '\0';
+// 	printf("j = %d\n", j);
+// 	i++;
+// 	while (j < tot)
+// 	{
+// 		//printf("here in\n");
+// 		new[j] = dir[i];
+// 		printf("(%c)\n", new[j]);
+// 		i++;
+// 		j++;
+// 		printf("save path\n");
+// 	}
+// 	printf("file == %s\n", new);
+// }
 
 //this method will be used to check for single files passed from other dirs
 void pathed_file(char *dirs)
@@ -82,12 +97,16 @@ void pathed_file(char *dirs)
 	if (path_(dirs))
 	{
 		char *path;
+		char *file;
 		char **content;
 
 		path = split_path(dirs);
-		//split_file(dirs); 
+		file = ft_strrchr(dirs, '/');
+		//split_file(dirs);
+		file++;
+		printf("==== %s\n", file);
 		printf(">>>>>%s\n", path);
-		content = save_(path);
+		//content = save_(path);
 	}
 	ft_putstr("ls: ");
 	ft_putstr(dirs);
